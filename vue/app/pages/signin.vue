6a3849f2a6675482aa3ec65c551e5d9e247d18f9
坂本一樹 <ikki@Tempest.local>
2019-04-23 17:51:25 +0900

    passwordのハッシュ化完了

--- vue/app/pages/signin.vue
+++ vue/app/pages/signin.vue
@@ -3,7 +3,7 @@
     <form @submit.prevent="login">
       <p class="error" v-if="error">{{ error }}</p>
       <p><input type="text" v-model="email" placeholder="email" name="email"/></p>
-      <p><input type="text" v-model="password" placeholder="password" name="password"/></p>
+      <p><input type="password" v-model="password" placeholder="password" name="password"/></p>
       <div class="login-btn">
         <button type="submit">ログイン</button>
       </div>
--- vue/app/store/actions.js
+++ vue/app/store/actions.js
@@ -1,9 +1,17 @@
+var bcrypt = require('bcryptjs');
+const saltRounds = 10; //ストレッチング回数
+
 export default {
   async login({ commit }, { email, password }) {
     console.log(commit)
     try {
+      //hashを生成して保存
+      var hashpass = bcrypt.hashSync(password, saltRounds); //saltとパスワードを設定してhashを生成
+      console.log(hashpass)
+
       // APIの通信。nustでリバプロしているので対象はnust自身
       // nust.config.jsで/apiをexpreeにルーティングしている
+      // リバプロしないとクロスドメイン制約に引っかかる。。。
       const response = await this.$axios.$get('http://localhost:8080/api/v1');
       let data = response;
 
